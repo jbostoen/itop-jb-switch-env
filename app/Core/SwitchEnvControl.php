baef8c24f57cf2c16e0bbf05fd373b759bb19cf7
Jeffrey Bostoen <support@jeffreybostoen.be>
2025-01-03 13:43:50 +0100

    * Fix: Page not reloaded if anchor present in URL.

--- app/Core/SwitchEnvControl.php
+++ app/Core/SwitchEnvControl.php
@@ -3,116 +3,53 @@
 	namespace jb_itop_extensions\switch_env;
 	
 	// iTop internals
-	use \AbstractPageUIBlockExtension;
-	use \Combodo\iTop\Application\UI\Base\Component\Html\Html;
-	use \iPageUIBlockExtension;
-	use \iTopWebPage;
-	use \MetaModel;
-		
-	// iTop 3+
-	if(defined('ITOP_VERSION') == true && version_compare(ITOP_VERSION, '3.0', '>=')) {
-		
-		
-		/**
-		 * Class SwitchEnvControl. Adds control to iTop backend top
-		 */
-		class SwitchEnvControl extends AbstractPageUIBlockExtension {
-		
-				
-			/**
-			 * @inheritDoc
-			 */
-			public function GetBannerBlock() {
-				
-				$sHTML =
-<<<HTML
-	
-				<div style="text-align: center; width: 100%;">
-					<select id="switch_env" onchange="javascript:var sEnv = $('#switch_env').val(); var sUrl = window.location.href.replace(/(&|\?)switch_env=.*?(?=&|$)/, ''); if(sUrl.includes('?') == false){ window.location.href = sUrl + '?switch_env=' + sEnv;} else { window.location.href = sUrl + '&switch_env=' + sEnv; }">
-HTML;
-			
-				$aEnvironments = glob(APPCONF.'/*', GLOB_ONLYDIR);
-				$sCurrentEnv = MetaModel::GetEnvironment();
-				
-				foreach($aEnvironments as $sEnv) {
-					
-					$sEnv = basename($sEnv);
-					$sSelected = ($sEnv == $sCurrentEnv ? 'selected' : '');
-					
-					$sHTML .=
-<<<HTML
-						<option value="{$sEnv}" {$sSelected}>{$sEnv}</option>
-HTML;
-				}
-			
-
-				$sHTML .=
-<<<HTML
-					</select>
-				</div>
-HTML;
-		
-				return new Html($sHTML);
-			
-			}
-
-			
-		}
-		
-	}
+	use AbstractPageUIBlockExtension;
+	use Combodo\iTop\Application\UI\Base\Component\Html\Html;
+	use MetaModel;
 	
 	/**
-	 * @todo Clean up as soon as 2.7 is phased out
+	 * Class SwitchEnvControl. Adds control to iTop backend top
 	 */
-	else {
-		
-		
-		
+	class SwitchEnvControl extends AbstractPageUIBlockExtension {
+	
+			
 		/**
-		 * Class SwitchEnvControl. Adds control to iTop backend top
+		 * @inheritDoc
 		 */
-		class SwitchEnvControl extends AbstractPageUIExtension {
-		
-				
-			/**
-			 * @inheritDoc
-			 */
-			public function GetBannerHtml(iTopWebPage $oPage) {
-				
-				$sAppVersion = ITOP_VERSION;
-				$sHTML =
-<<<HTML
-					<!-- iTop version: {$sAppVersion} -->
-					<div style="text-align: center; width: 100%;">
-						<select id="switch_env" onchange="javascript:var sEnv = $('#switch_env').val(); var sUrl = window.location.href.replace(/(&|\?)switch_env=.*?(?=&|$)/, ''); if(sUrl.includes('?') == false){ window.location.href = sUrl + '?switch_env=' + sEnv;} else { window.location.href = sUrl + '&switch_env=' + sEnv; }">
-HTML;
-				
-				$aEnvironments = glob(APPCONF.'/*', GLOB_ONLYDIR);
-				$sCurrentEnv = MetaModel::GetEnvironment();
-				
-				foreach($aEnvironments as $sEnv) {
-					
-					$sEnv = basename($sEnv);
-					$sSelected = ($sEnv == $sCurrentEnv ? 'selected' : '');
-					
-					$sHTML .=
-<<<HTML
-							<option value="{$sEnv}" {$sSelected}>{$sEnv}</option>
-HTML;
-				}
-				
+		public function GetBannerBlock() {
+			
+			$sHTML =
+				<<<HTML
 
+					<div style="text-align: center; width: 100%;">
+						<select id="switch_env" onchange="javascript:let sEnv = $('#switch_env').val(); let sUrl = window.location.href.replace(/(&|\?)switch_env=.*?(?=&|$)/, ''); if(sUrl.includes('?') == false){ window.location.href = sUrl + '?switch_env=' + sEnv;} else { window.location.href = sUrl.replace('?', '?&switch_env=' + sEnv); }">
+				HTML;
+		
+			$aEnvironments = glob(APPCONF.'/*', GLOB_ONLYDIR);
+			$sCurrentEnv = MetaModel::GetEnvironment();
+			
+			foreach($aEnvironments as $sEnv) {
+				
+				$sEnv = basename($sEnv);
+				$sSelected = ($sEnv == $sCurrentEnv ? 'selected' : '');
+				
 				$sHTML .=
-<<<HTML
+					<<<HTML
+							<option value="{$sEnv}" {$sSelected}>{$sEnv}</option>
+					HTML;
+			}
+		
+
+			$sHTML .=
+				<<<HTML
 						</select>
 					</div>
-HTML;
-			
-				return $sHTML;
-				
-			}
-
+				HTML;
+	
+			return new Html($sHTML);
 		
 		}
+
+		
+}
 	
-	}
--- en.dict.jb-switch-env.php
+++ en.dict.jb-switch-env.php
@@ -9,12 +9,7 @@
 
 Dict::Add('EN US', 'English', 'English', array(
 
-	//	'Class:SomeClass' => 'Class name',
-	//	'Class:SomeClass+' => 'More info on class name',
-	//	'Class:SomeClass/Attribute:some_attribute' => 'your translation for the label',
-    //	'Class:SomeClass/Attribute:some_attribute/Value:some_value' => 'your translation for a value',
-    //	'Class:SomeClass/Attribute:some_attribute/Value:some_value+' => 'your translation for more info on the value',
-    //	'SomeClass/Some:Fieldset' => 'Fieldset',
+	'UI:Report:SuggestedArticles:ReportSearchSessionGuids:Overview' => 'Report',
 	
 ));
 
